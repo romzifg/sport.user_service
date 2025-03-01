9cfc84d610816b188cdc127619aeb67cd272ea60
Romzi Farhan Ghozi <romzifg@gmail.com>
2025-03-01 15:28:08 +0700

    update: add user repository

--- repositories/user/user.go
+++ repositories/user/user.go
@@ -19,6 +19,7 @@ type UserRepository struct {
 type IUserRepository interface {
 	Register(context.Context, *dto.RegiterRequest) (*models.User, error)
 	Update(context.Context, *dto.UpdateRequest, string) (*models.User, error)
+	UpdatePassword(context.Context, *dto.UpdatePasswordRequest, string) (*models.User, error)
 	FindByUsername(context.Context, string) (*models.User, error)
 	FindByEmail(context.Context, string) (*models.User, error)
 	FindByUUID(context.Context, string) (*models.User, error)
@@ -52,8 +53,20 @@ func (r *UserRepository) Update(ctx context.Context, req *dto.UpdateRequest, uui
 		Name:        req.Name,
 		Username:    req.Username,
 		Email:       req.Email,
-		Password:    req.Password,
 		PhoneNumber: req.PhoneNumber,
+	}
+
+	err := r.db.WithContext(ctx).Where("uuid = ?", uuid).Updates(user).Error
+	if err != nil {
+		return nil, wrapError.WrapError(errConstant.ErrSqlError)
+	}
+
+	return user, nil
+}
+
+func (r *UserRepository) UpdatePassword(ctx context.Context, req *dto.UpdatePasswordRequest, uuid string) (*models.User, error) {
+	user := &models.User{
+		Password: req.NewPassword,
 	}
 
 	err := r.db.WithContext(ctx).Where("uuid = ?", uuid).Updates(user).Error
