57900a2ca714c4001e9eb0ec55c697b848b8a06e
Romzi Farhan Ghozi <romzifg@gmail.com>
2025-03-02 14:19:42 +0700

    update: update middleware

--- middlewares/middleware.go
+++ middlewares/middleware.go
@@ -1,15 +1,23 @@
 package middlewares
 
 import (
+	"context"
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
 	"net/http"
+	"strings"
 	"user-service/common/response"
+	"user-service/config"
 	"user-service/constants"
+	services "user-service/services/user"
 
 	errConstants "user-service/constants/error"
 
 	"github.com/didip/tollbooth"
 	"github.com/didip/tollbooth/limiter"
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/sirupsen/logrus"
 )
 
@@ -39,6 +47,97 @@ func RateLimiter(lmt *limiter.Limiter) gin.HandlerFunc {
 			})
 			c.Abort()
 		}
+		c.Next()
+	}
+}
+
+func extractBearerToken(token string) string {
+	arrayToken := strings.Split(token, " ")
+	if len(arrayToken) == 2 {
+		return arrayToken[1]
+	}
+
+	return ""
+}
+
+func responseUnauthorized(c *gin.Context, message string) {
+	c.JSON(http.StatusUnauthorized, response.Response{
+		Status:  constants.Error,
+		Message: message,
+	})
+	c.Abort()
+}
+
+func validateApiKey(c *gin.Context) error {
+	apiKey := c.GetHeader(constants.XApiKey)
+	requestAt := c.GetHeader(constants.XRequestAt)
+	serviceName := c.GetHeader(constants.XServiceName)
+	signatureKey := config.Config.SignatureKey
+
+	validateKey := fmt.Sprintf("%s:%s:%s", serviceName, signatureKey, requestAt)
+	hash := sha256.New()
+	hash.Write([]byte(validateKey))
+	resultHash := hex.EncodeToString(hash.Sum(nil))
+
+	if apiKey != resultHash {
+		return errConstants.ErrUnauthorized
+	}
+
+	return nil
+}
+
+func validateBearerToken(c *gin.Context, token string) error {
+	if !strings.Contains(token, "Bearer") {
+		return errConstants.ErrUnauthorized
+	}
+
+	tokenString := extractBearerToken(token)
+	if tokenString == "" {
+		return errConstants.ErrUnauthorized
+	}
+
+	claims := &services.Claims{}
+	tokenJwt, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+		if !ok {
+			return nil, errConstants.ErrInvalidToken
+		}
+		jwtScret := []byte(config.Config.JwtSecret)
+		return jwtScret, nil
+	})
+
+	if err != nil || !tokenJwt.Valid {
+		return errConstants.ErrUnauthorized
+	}
+
+	type contextKey string
+	userLogin := c.Request.WithContext(context.WithValue(c.Request.Context(), contextKey(constants.UserLogin), claims.User))
+	c.Request = userLogin
+	c.Set(constants.Token, token)
+	return nil
+}
+
+func Authenticate() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var err error
+		token := c.GetHeader(constants.Authorization)
+		if token != "" {
+			responseUnauthorized(c, errConstants.ErrUnauthorized.Error())
+			return
+		}
+
+		err = validateBearerToken(c, token)
+		if err != nil {
+			responseUnauthorized(c, errConstants.ErrUnauthorized.Error())
+			return
+		}
+
+		err = validateApiKey(c)
+		if err != nil {
+			responseUnauthorized(c, errConstants.ErrUnauthorized.Error())
+			return
+		}
+
 		c.Next()
 	}
 }
