dc2eaa3024a0de33e5cecf03bfd39318983a1b69
Romzi Farhan Ghozi <romzifg@gmail.com>
2025-03-03 12:43:05 +0700

    update: add command for running app

---
+++ cmd/main.go
@@ -0,0 +1,106 @@
+package cmd
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+	"user-service/common/response"
+	"user-service/config"
+	"user-service/constants"
+	"user-service/controllers"
+	"user-service/database/seeder"
+	"user-service/domain/models"
+	"user-service/middlewares"
+	"user-service/repositories"
+	"user-service/routes"
+	"user-service/services"
+
+	"github.com/didip/tollbooth"
+	"github.com/didip/tollbooth/limiter"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"github.com/spf13/cobra"
+)
+
+var command = &cobra.Command{
+	Use:   "serve",
+	Short: "Start the server",
+	Run: func(c *cobra.Command, args []string) {
+		_ = godotenv.Load()
+		config.Init()
+		db, err := config.InitDatabase()
+		if err != nil {
+			panic(err)
+		}
+
+		loc, err := time.LoadLocation("Asia/Jakarta")
+		if err != nil {
+			panic(err)
+		}
+		time.Local = loc
+
+		err = db.AutoMigrate(
+			&models.Role{},
+			&models.User{},
+		)
+		if err != nil {
+			panic(err)
+		}
+
+		seeder.NewSeederRegistry(db).Run()
+		repository := repositories.NewRepositoryRegistry(db)
+		service := services.NewServiceRegistry(repository)
+		controller := controllers.NewControllerRegistry(service)
+
+		router := gin.Default()
+		router.Use(middlewares.HandlePanic())
+		router.NoRoute(func(c *gin.Context) {
+			c.JSON(http.StatusNotFound, response.Response{
+				Status:  constants.Error,
+				Message: fmt.Sprintf("Path %s", http.StatusText(http.StatusNotFound)),
+			})
+		})
+
+		router.GET("/", func(c *gin.Context) {
+			c.JSON(http.StatusOK, response.Response{
+				Status:  constants.Success,
+				Message: "Welcome to User Service",
+			})
+		})
+
+		router.Use(func(c *gin.Context) {
+			c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+			c.Writer.Header().Set("Access-Control-Allow-Method", "GET, POST, PUT")
+			c.Writer.Header().Set("Access-Control-Allow-Header", "Content-Type, Authorization, x-service-name, x-api-key, x-request-at")
+			c.Next()
+		})
+
+		router.GET("/health", func(c *gin.Context) {
+			c.JSON(http.StatusOK, response.Response{
+				Status:  constants.Success,
+				Message: "OK",
+			})
+		})
+
+		lmt := tollbooth.NewLimiter(
+			config.Config.RateLimitMaxRequest,
+			&limiter.ExpirableOptions{
+				DefaultExpirationTTL: time.Duration(config.Config.RateLimitTimeSecond) * time.Second,
+			},
+		)
+		router.Use(middlewares.RateLimiter(lmt))
+		group := router.Group("/api/v1")
+		route := routes.NewRouteRegistry(controller, group)
+		route.Serve()
+
+		port := fmt.Sprintf(":%d", config.Config.Port)
+		router.Run(port)
+	},
+}
+
+func Run() {
+	err := command.Execute()
+	if err != nil {
+		panic(err)
+	}
+}
